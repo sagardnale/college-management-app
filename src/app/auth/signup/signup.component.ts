2908eaadfea4ea93a066e0da4b9b5e092aa8f332
sagardnale <nalesagard@gmail.com>
2025-08-20 17:37:33 +0530

    added UI and functionality for Signup page

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+ signupForm = this.fb.group({
+  name:['',[Validators.required,Validators.minLength(3)]],
+  email:['',[Validators.required,Validators.email]],
+  password:['',[Validators.required,Validators.minLength(6)]],
+  role:['',[Validators.required]],
+  className:['']
+ })
+  constructor(private readonly fb: FormBuilder){
 
+ }
+ get f(){
+  return this.signupForm.controls;
+ }
+ onSignup(){
+  console.log(this.signupForm.value);
+ }
 }
