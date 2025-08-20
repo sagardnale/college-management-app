f0ad1e6f1f0deae57083bb4546b3347b4f728f93
sagardnale <nalesagard@gmail.com>
2025-08-20 18:10:33 +0530

    added routing to dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { PrincipalDashboardComponent } from './dashboard/principal-dashboard/principal-dashboard.component';
+import { TeacherDashboardComponent } from './dashboard/teacher-dashboard/teacher-dashboard.component';
+import { StudentDashboardComponent } from './dashboard/student-dashboard/student-dashboard.component';
 
 const routes: Routes = [
   {path:"",component:LoginComponent},
   {path:"login",component:LoginComponent},
-  {path:"signup",component:SignupComponent}
+  {path:"signup",component:SignupComponent},
+  {path:"principal",component:PrincipalDashboardComponent},
+  {path:"teacher",component:TeacherDashboardComponent},
+  {path:"student",component:StudentDashboardComponent}
 ];
 
 @NgModule({
